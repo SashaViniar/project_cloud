0fea55c46510c7a897d565a8e68a938fe3ad7256
SashaViniar <vinar262@gmail.com>
2016-11-19 18:52:28 +0200

    Shit Commit.#8

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 import Task from './Task.jsx';
  
 // App component - represents the whole app
-export default class App extends Component {
-  getTasks() {   
-  }
+class App extends Component {
  
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return  this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
