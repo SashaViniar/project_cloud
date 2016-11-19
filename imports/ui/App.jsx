dbf349675ec458e539ea0694b80d2eaa879c4e99
SashaViniar <vinar262@gmail.com>
2016-11-19 19:01:38 +0200

    Shit Commit.#8

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+ import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
  
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
  
 // App component - represents the whole app
