2be2c00e0ac734cdb03c89bb7a1afb089a38bb7d
Dudyak Michael <sergolwow@gmail.com>
2017-03-03 23:46:42 +0200

    register

--- imports/ui/Register.jsx
+++ imports/ui/Register.jsx
@@ -16,22 +16,17 @@ class Register extends React.Component {
 					</div>
 				</div>
 				<div className="row centr">
-					<h4 className="light-font">Authentication:</h4>
+					<h4 className="light-font">Register:</h4>
 				</div>
 				<div className="row">
-					<div className="col-md-2 col-xs-1"></div>
-					<div className="col-md-8 col-xs-10 dashed-border">
-						
 						<div className="row vcentr">
-							<div className="col-md-5">
+							<div className="col-md-4"></div>
+							<div className="col-md-4 dashed-border">
+								<div className="row" style={{"height":"1em"}}></div>
 								<RegisterUIWrapper/>
 							</div>
-							<div className="col-md-2 centr light-font">
-								<h2 className="m0">OR</h2>
-							</div>
-							
+							<div className="col-md-4"></div>
 						</div>
-					</div>
 					<div className="col-md-2 col-xs-1"></div>
 				</div>
 				<div className="row" style={{"height":"1em"}}></div>
