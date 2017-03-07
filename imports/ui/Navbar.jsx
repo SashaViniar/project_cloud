936a5686b6d625fe46b9ce2d34be260dd4316b28
Dudyak Michael <sergolwow@gmail.com>
2017-03-07 13:41:56 +0200

    Navbar refactoring

--- imports/ui/Navbar.jsx
+++ imports/ui/Navbar.jsx
@@ -1,68 +1,97 @@
 import React from 'react';
+
+var MenuItemLink = (props) => {
+  return(
+    <li className="menu__item"><a className="menu__link" href="#">{props.caption}</a></li>
+  );
+}
+
+var SubMenuLink = (props) => {
+  return(
+    <li className="menu__item"><a className="menu__link" data-submenu={props.submenu} href="#">{props.caption}</a></li>
+  );
+}
+
+var Menu = (props) => {
+  return(
+    <ul data-menu={props.name} className="menu__level">
+      {
+        props.items.map(x=>
+          x[0]=="submenu"?
+            <SubMenuLink submenu={x[2]} caption={x[1]} />
+          :
+            <MenuItemLink caption={x[1]} />
+        )
+      }
+    </ul>
+  );
+}
+
 class Navbar extends React.Component {
   render(){
   	return(
 	    <nav id="ml-menu" className="menu">
           <button className="action action--close" aria-label="Close Menu"><span className="icon icon--cross"></span></button>
           <div className="menu__wrap">
-            <ul data-menu="main" className="menu__level">
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-1" href="#">Vegetables</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-2" href="#">Fruits</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-3" href="#">Grains</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-4" href="#">Mylk &amp; Drinks</a></li>
-            </ul>
-            <ul data-menu="submenu-1" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Stalk Vegetables</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Roots &amp; Seeds</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Cabbages</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Salad Greens</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Mushrooms</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-1-1" href="#">Sale %</a></li>
-            </ul>
-            <ul data-menu="submenu-1-1" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Fair Trade Roots</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Dried Veggies</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Our Brand</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Homemade</a></li>
-            </ul>
-            <ul data-menu="submenu-2" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Citrus Fruits</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Berries</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-2-1" href="#">Special Selection</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Tropical Fruits</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Melons</a></li>
-            </ul>
-            <ul data-menu="submenu-2-1" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Exotic Mixes</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Wild Pick</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Vitamin Boosters</a></li>
-            </ul>
-            <ul data-menu="submenu-3" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Buckwheat</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Millet</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Quinoa</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Wild Rice</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Durum Wheat</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-3-1" href="#">Promo Packs</a></li>
-            </ul>
-            <ul data-menu="submenu-3-1" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Starter Kit</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">The Essential 8</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Bolivian Secrets</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Flour Packs</a></li>
-            </ul>
-            <ul data-menu="submenu-4" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Grain Mylks</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Seed Mylks</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Nut Mylks</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Nutri Drinks</a></li>
-              <li className="menu__item"><a className="menu__link" data-submenu="submenu-4-1" href="#">Selection</a></li>
-            </ul>
-            <ul data-menu="submenu-4-1" className="menu__level">
-              <li className="menu__item"><a className="menu__link" href="#">Nut Mylk Packs</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Amino Acid Heaven</a></li>
-              <li className="menu__item"><a className="menu__link" href="#">Allergy Free</a></li>
-            </ul>
+            <Menu name="main" items={[
+              ["submenu", "Vegetables", "submenu-1"],
+              ["submenu", "Fruits", "submenu-2"],
+              ["submenu", "Grains", "submenu-3"],
+              ["submenu", "Mylk & Drinks", "submenu-4"],
+              ["link", "Logout"]
+            ]} />
+            <Menu name="submenu-1" items={[
+              ["link", "Stalk Vegetables"],
+              ["link", "Roots & Seeds"],
+              ["link", "Cabbages"],
+              ["link", "Salad Greens"],
+              ["link", "Mushrooms"],
+              ["submenu", "Sale %", "submenu-1-1"]
+            ]} />
+            <Menu name="submenu-1-1" items={[
+              ["link", "Fair Trade Roots"],
+              ["link", "Dried Veggies"],
+              ["link", "Our Brand"],
+              ["link", "Homemade"]
+            ]} />
+            <Menu name="submenu-2" items={[
+              ["link", "Citrus Fruits"],
+              ["link", "Berries"],
+              ["submenu", "Special Selection", "submenu-2-1"],
+              ["link", "Tropical Fruits"],
+              ["link", "Melons"]
+            ]} />
+            <Menu name="submenu-2-1" items={[
+              ["link", "Exotic Mixes"],
+              ["link", "Wild Pick"],
+              ["link", "Vitamin Boosters"]
+            ]} />
+            <Menu name="submenu-3" items={[
+              ["link", "Buckwheat"],
+              ["link", "Millet"],
+              ["link", "Quinoa"],
+              ["link", "Wild Rice"],
+              ["link", "Durum Wheat"],
+              ["submenu", "Promo Packs", "submenu-3-1"]
+            ]} />
+            <Menu name="submenu-3-1" items={[
+              ["link", "Starter Kit"],
+              ["link", "The Essential 8"],
+              ["link", "Bolivian Secrets"],
+              ["link", "Flour Packs"]
+            ]} />
+            <Menu name="submenu-4" items={[
+              ["link", "Grain Mylks"],
+              ["link", "Seed Mylks"],
+              ["link", "Nut Mylks"],
+              ["link", "Nutri Drinks"],
+              ["submenu", "Selection", "submenu-4-1"]
+            ]} />
+            <Menu name="submenu-4-1" items={[
+              ["link", "Nut Mylk Packs"],
+              ["link", "Amino Acid Heaven"],
+              ["link", "Allergy Free"]
+            ]} />
           </div>
         </nav>
 	 );
--- imports/ui/Register.jsx
+++ imports/ui/Register.jsx
@@ -1,61 +1,62 @@
 import React from 'react';
 import RegisterUIWrapper from './RegisterUIWrapper.jsx';
+import { Accounts } from 'meteor/accounts-base';
 
 // Register component - represents the whole app
 class Register extends React.Component {
-	constructor() {
-		super();
-		Template.register.events({
-			'click #signup': function(e) {
-				e.preventDefault();
-				var username = $("#login").val();
-				var email = $("#email").val();
-				var password = $("#password").val();
-				Meteor.createUser(username, email,password, error=>{
-					if(error) {
-						alert(error);
-					} else {
-						FlowRouter.go('/');
-					}
-				});
-			},
-			'click #signin': function(e) {
-				e.preventDefault();
-				FlowRouter.go('/login');
-			}
-		});
-	}
-	render() {
-		return (
-			<div>
-				<div className="row" style={{"height":"1em"}}></div>
-				<div className="row centr">
-					<div className="row">
-						<img src="/logo.png" width="300" alt="" />
-					</div>
-					<div className="row loginHeading light-font">
-						<h1>Project Cloud</h1>
-					</div>
-				</div>
-				<div className="row centr">
-					<h4 className="light-font">Register:</h4>
-				</div>
-				<div className="row">
-						<div className="row vcentr">
-							<div className="col-md-4"></div>
-							<div className="col-md-4 dashed-border">
-								<div className="row" style={{"height":"1em"}}></div>
-								<RegisterUIWrapper/>
-							</div>
-							<div className="col-md-4"></div>
-						</div>
-					<div className="col-md-2 col-xs-1"></div>
-				</div>
-				<div className="row" style={{"height":"1em"}}></div>
-			</div>
-		);
-	}
+  constructor() {
+    super();
+    Template.register.events({
+      'click #signup': function(e) {
+        e.preventDefault();
+        var username = $("#login").val();
+        var email = $("#email").val();
+        var password = $("#password").val();
+        Meteor.createUser(username, email, password, error=>{
+          if(error) {
+            alert(error);
+          } else {
+            FlowRouter.go('/');
+          }
+        });
+      },
+      'click #signin': function(e) {
+        e.preventDefault();
+        FlowRouter.go('/login');
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div className="row" style={{"height":"1em"}}></div>
+        <div className="row centr">
+          <div className="row">
+            <img src="/logo.png" width="300" alt="" />
+          </div>
+          <div className="row loginHeading light-font">
+            <h1>Project Cloud</h1>
+          </div>
+        </div>
+        <div className="row centr">
+          <h4 className="light-font">Register:</h4>
+        </div>
+        <div className="row">
+            <div className="row vcentr">
+              <div className="col-md-4"></div>
+              <div className="col-md-4 dashed-border">
+                <div className="row" style={{"height":"1em"}}></div>
+                <RegisterUIWrapper/>
+              </div>
+              <div className="col-md-4"></div>
+            </div>
+          <div className="col-md-2 col-xs-1"></div>
+        </div>
+        <div className="row" style={{"height":"1em"}}></div>
+      </div>
+    );
+  }
 }
-				
+        
 
 export default Register;
