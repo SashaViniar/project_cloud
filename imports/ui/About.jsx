7010659f44c6c5c66939295036d42c922911c169
Dudyak Michael <sergolwow@gmail.com>
2017-05-14 18:59:24 +0300

    Fixed about

--- imports/ui/About.jsx
+++ imports/ui/About.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+const ids = [
+  ["mnxoid", "Dudyak Michael"], 
+  ["lilia", "Matyaschuk Liliya"], 
+  ["sasha", "Olexandr Viniar"]
+];
+
 class About extends React.Component {
   
   render(){
@@ -11,41 +17,23 @@ class About extends React.Component {
           <h1>About:</h1>
         </header>
         <div className="col-md-12">
-        	<div className="col-md-4">
-        		<div className="with-img">
-        			 <img  width = "100%" src="mnxoid.jpg" alt=""/>
-        		</div>
-        		<div className="row">
-        			<div className="text-with-description">
-        				
-        			</div>
-        		</div>
-        		<div className="row">
-        			<div className="link-social"></div>
-        		</div>
-        	</div>
-        	<div className="col-md-4">
-        		<div className="with-img">
-        			<img  width = "100%" src="lilia.jpg" alt=""/>
-        		</div>
-        		<div className="row">
-        			<div className="text-with-description">
-        				MATYASHCHUK LILIYA
-        			</div>
-        		</div>
-        		<div className="row">
-        			<div className="link-social"></div>
-        		</div>
-        	</div>
-        	<div className="col-md-4">
-        		<img  width = "100%" src="sasha.jpg" alt=""/>
-        		<div className="row">
-        			<div className="text-with-description">VINIAR OLEKSANDR</div>
-        		</div>
-        		<div className="row">
-        			<div className="link-social"></div>
-        		</div>
-        	</div>
+          {
+            ids.map(id =>
+              <div className="col-md-4">
+                <div>
+                   <img style={{display:"block"}} width="100%" src={id[0]+".jpg"} alt=""/>
+                </div>
+                <div className="row">
+                  <div className="text-with-description text-center">
+                    {id[1]}
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="link-social"></div>
+                </div>
+              </div>
+            )
+          }
         </div>
         
       </div>
