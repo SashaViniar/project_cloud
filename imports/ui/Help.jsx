779f0fc2eee832cb8367af8795ce92154c1fddd0
SashaViniar <vinar262@gmail.com>
2017-05-14 19:16:09 +0300

    html for help

--- imports/ui/Help.jsx
+++ imports/ui/Help.jsx
@@ -8,9 +8,44 @@ class Help extends React.Component {
     return(
       <div className={this.props.className}>
         <header>
-          <h1>Help:</h1>
+          <h1>About:</h1>
         </header>
-        
+        <div className="col-md-12">
+        	<div className="col-md-1"></div>
+        	<div className="col-md-10">
+        		<p className = "text-help"></p>
+        	</div>
+        	<div className="col-md-1"></div>
+        </div>
+
+        <div className="col-md-12">
+        	<h1>Operator</h1>
+        	<table>
+        		<tbody>
+        			<tr className = "col-md-3">
+        				<td></td>
+        			</tr>
+        			<tr className = "col-md-9">
+        				<td></td>
+        			</tr>
+        		</tbody>
+        	</table>
+        </div>
+
+
+        <div className="col-md-12">
+        	<h1>Function</h1>
+        	<table>
+        		<tbody>
+        			<tr className = "col-md-3">
+        				<td></td>
+        			</tr>
+        			<tr className = "col-md-9">
+        				<td></td>
+        			</tr>
+        		</tbody>
+        	</table>
+        </div>
       </div>
     );
   }
